7fbfb87a1c2c2a1eab101de3b33973f810cec446
Krishnaba79 <solankikrishnabahitendrasinh@gmail.com>
2025-04-29 22:39:59 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,25 @@
+# Comments can be used to explain Python code.
+# Comments can be used to make the code more readable.
+# Comments can be used to prevent execution when testing code
+
+# Examples
+#This is a comment
+print("Hello, World!")
+
+print("Hello, World!") #This is a comment
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+
+# Multiline Comments
+"""""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+# Exercise
+# Which character is used to define a Python comment:
+# ans: #
---
+++ gettingstarted.py
@@ -0,0 +1,5 @@
+print("Hello my name is Solanki Krishnaba!")
+# Check the Python version of the editor:
+
+import sys
+print(sys.version)
