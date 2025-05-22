b4f0399c3ad9e3ea9718b25efadd86993f4dd12a
Krishnaba79 <solankikrishnabahitendrasinh@gmail.com>
2025-05-22 19:42:26 +0530

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+firstname = "Tony"
+lastname= "Shark"
+age=51
+is_genius=True
+
+print(firstname,lastname)
+print(age)
+print(is_genius)
---
+++ variables.py
@@ -0,0 +1,16 @@
+name = "Sharadha"
+age = 22
+
+name= "Aman"
+age=24.0 
+
+is_adult = True
+
+
+age1=16
+is_adult = False
+
+
+
+print(name)
+print(age)
