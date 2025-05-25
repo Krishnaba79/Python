84d0203427825a20b4ad271ac40dd6d8a48d260a
Krishnaba79 <solankikrishnabahitendrasinh@gmail.com>
2025-05-25 11:28:43 +0530

    Add files via upload

---
+++ printevenorodd.py
@@ -0,0 +1,10 @@
+# Print Even or Odd
+
+# Write a Python program that takes an integer input from the user and prints whether it’s even or odd.
+
+firstnum=int(input("Enter you First Number:"))
+if firstnum % 2 == 0 :
+    print("This number is even!")
+else:
+    print("This number is odd!")
+print("Thank You!")
